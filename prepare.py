130dd15b447b2db2165cad4c1b62f151caf43fcc
rachelrobbinsmayhill <rachel.robbinsmayhill@gmail.com>
2022-03-14 11:56:00 -0500

    Complete

--- prepare.py
+++ prepare.py
@@ -56,34 +56,43 @@ def clean_telco_data(df):
     # combine df and dummy_df that I created
     df = pd.concat([df, dummy_df], axis =1)
 
+    columns_to_drop = ['is_male',
+               'has_partner',
+               'has_dependents',
+               'has_phone_service',
+               'has_multiple_lines',
+               'has_online_security',
+               'has_online_backup',
+               'has_device_protection',
+               'has_tech_support',
+               'has_streaming_tv',
+               'has_streaming_movies',
+               'has_paperless_billing',
+               'did_churn',
+              ]
+    
     # dropping columns that are now duplicates, from the dummies created, or not needed
-    df = df.drop(columns = columns)
+    df = df.drop(columns = columns_to_drop)
     df = df.drop(columns = ['payment_type_id', 'internet_service_type_id', 'contract_type_id'])
 
-    
-    
     return df     
 
 
 
 
-
-def data_split(df, random_state=123):
+def data_split(df):
     '''
-    This function splits a dataframe into train, validate, and test 
-    in order to create and validate models. It takes in a dataframe. The  
-    target variable (for stratification purposes) is set within the function, 
+    This function drops the customer_id column and then splits a dataframe into 
+    train, validate, and test in order to explore the data and to create and validate models. 
+    It takes in a dataframe. The target variable (for stratification purposes) is set within the function, 
     and contains an integer for setting a seed for replication. 
     Test is 20% of the original dataset. The remaining 80% of the dataset is 
     divided between valiidate and train, with validate being .30*.80= 24% of 
     the original dataset, and train being .70*.80= 56% of the original dataset. 
     The function returns, train, validate and test dataframes. 
     '''
-    
-     
-    train, test = train_test_split(df, test_size = .2, random_state=123, stratify=df.did_churn_Yes)
-    
-   
+    df = df.drop(columns=['customer_id'])
+    train, test = train_test_split(df, test_size = .2, random_state=123, stratify=df.did_churn_Yes)   
     train, validate = train_test_split(train, test_size=.3, random_state=123, stratify=train.did_churn_Yes)
 
 
@@ -91,7 +100,6 @@ def data_split(df, random_state=123):
     print(f'validate -> {validate.shape}')
     print(f'test -> {test.shape}')
     
-    # results in 3 dataframes
     return train, validate, test
 
 
